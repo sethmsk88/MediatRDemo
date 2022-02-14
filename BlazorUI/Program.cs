f7c9806517b9cbc9a41e8b99f8c08ef8003eb1b8
Seth Kerr <Seth_Kerr@carmax.com>
2022-02-14 18:24:30 -0500

    Add project files.

---
+++ BlazorUI/Program.cs
@@ -0,0 +1,40 @@
+using BlazorUI.Data;
+using DemoLibrary;
+using DemoLibrary.DataAccess;
+using MediatR;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+builder.Services.AddServerSideBlazor();
+builder.Services.AddSingleton<WeatherForecastService>();
+builder.Services.AddSingleton<IDataAccess, DemoDataAccess>();
+
+
+// The following code starting with typeof, uses reflection to load the DemoDataAccess assembly.
+// typeof(DemoDataAcess).Assembly
+builder.Services.AddMediatR(typeof(DemoLibraryMediatREntryPoint).Assembly);
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.MapBlazorHub();
+app.MapFallbackToPage("/_Host");
+
+app.Run();
---
+++ DemoLibrary/Queries/GetPersonListQuery.cs
@@ -0,0 +1,19 @@
+﻿using DemoLibrary.Models;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoLibrary.Queries
+{
+    // Records are like classes, but they are immutable by default
+    public record GetPersonListQuery() : IRequest<List<PersonModel>>;
+
+    // This class definition is basically the same as the Record definition above, except it doesn't have the benefits of being a Record type
+    /*public class GetPersonListQueryClass : IRequest<List<PersonModel>>
+    {
+
+    }*/
+}
